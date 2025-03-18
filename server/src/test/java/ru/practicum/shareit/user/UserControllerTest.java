a215c723e06ff859a67675bef562f2d555c44751
Damir Minigaliev <dminigaliev@gmail.com>
2025-03-18 18:43:17 +0500

    ТЗ 16, доавбил тесты контроллера ItemRequest

--- server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
