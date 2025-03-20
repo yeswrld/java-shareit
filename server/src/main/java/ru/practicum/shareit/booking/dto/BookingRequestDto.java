6c24904145f5a5e3a929eb0c74e94c3865112f92
Damir Minigaliev <dminigaliev@gmail.com>
2025-03-20 17:15:13 +0500

    ТЗ 16, доавбил тесты

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
